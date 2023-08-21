46c675d7338809f8dce11848df0f1f8280107e4f
Sumardi <sumardi@rekadia.co.id>
2023-08-21 10:57:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const numberInput = useRef(null);
+  const [result, setresult] = useState();
+
+  const segitigaClick = (e) => {
+    e.preventDefault();
+    let number = numberInput.current.value;
+    if(number <= 0){
+      setresult("Number must be greater than 0");
+    }
+    else{
+      let input = number + '';
+      let result = '';
+      for(let i = 0; i < input.length; i++){
+        result += (input[i] + '');
+        for(let j = 1; j < (i+2); j++){
+          result += ('0');
+        }
+        result += ('\n');
+      }
+      setresult(result.split('\n').map(str => <p>{str}</p>));
+    }
+  }
+
+  const ganjilClick = (e) => {
+    e.preventDefault();
+    let number = numberInput.current.value;
+    if(number <= 0){
+      setresult("Number must be greater than 0");
+    }
+    else{
+      let result = '';
+      for(let i = 1; i <= number; i++){
+        if(i%2 != 0){
+          result += (i + ' ');
+        }
+      }
+      setresult(result);
+    }
+  }
+
+  const primaClick = (e) => {
+    e.preventDefault();
+    let number = numberInput.current.value;
+    if(number <= 0){
+      setresult("Number must be greater than 0");
+    }
+    else{
+      let count = 0;
+      let result = '';
+      for(let i = 2; i <= number; i++){
+        for(let j = 1; j <= i; j++){
+          if(i % j == 0){
+            count++;
+          }
+        }
+        if(count == 2){
+          result += (i + ' ');
+        }
+        count = 0;
+      }
+      setresult(result);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <label>Input Angka</label>
+      <input ref={numberInput}/>
+      <br></br>
+      <button onClick={segitigaClick}>Generate segitiga</button>
+      <button onClick={ganjilClick}>Generate Bilangan Ganjil</button>
+      <button onClick={primaClick}>Generate Bilangan Prima</button>
+
+      <p>Result: {result}</p>
+    </form>
   );
 }
 
